c29a5a25c5191416af8e0135974acfb76a8ad2fa
yang-dal <yuop1022@gmail.com>
2024-02-02 16:47:25 +0900

    백준 13458 번 문제 IOException 추가.

--- Baekjoon/week_01/Inspector.java
+++ Baekjoon/week_01/Inspector.java
@@ -1,23 +1,24 @@
 package Baekjoon.week_01;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Inspector {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner scan = new Scanner(System.in);
         int N = scan.nextInt();
 
         ArrayList<Integer> A = new ArrayList<>();
-        for (int i=1; i<=N; i++) {
+        for (int i = 1; i <= N; i++) {
             A.add(scan.nextInt());
         }
 
         int B = scan.nextInt(), C = scan.nextInt();
         int totalInspectorCount = 0;
 
-        for (int i=1; i<=N; i++) {
-            int candidateNum = A.get(i-1);
+        for (int i = 1; i <= N; i++) {
+            int candidateNum = A.get(i - 1);
             candidateNum -= B;
             totalInspectorCount += 1;
 
