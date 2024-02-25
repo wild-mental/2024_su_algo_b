47348ffcc84c6eb2627bf5ea8dc078acaa91a992
c10aking <grkim43@gmail.com>
2024-02-25 19:40:29 +0900

    코드 수정

--- Baek1100_B2.py
+++ Baek1100_B2.py
@@ -1,10 +1,13 @@
 count = 0
-board = input().split()
 
-for i in range(64):
-    row = i // 8 # 행
-    col = i % 8 # 열
-
-    if (row + col) % 2 == 0 and board[row][col] == "F": # 조건 만족시 카운팅
-        count += 1
+for i in range(8) :
+    horse = input()
+    if i % 2 == 0 :
+        for j in range(0, 8, 2) : #짝수행
+            if horse[j] == 'F' :
+                count += 1
+    elif i % 2 != 0 :
+        for j in range(1, 8, 2) : #홀수행
+            if horse[j] == 'F' :
+                count += 1
 print(count)
