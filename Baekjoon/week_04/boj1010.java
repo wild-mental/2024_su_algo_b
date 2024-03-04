b9bbc4905224ada6c2d7ef1f758f247556927488
yang-dal <yuop1022@gmail.com>
2024-03-04 22:39:57 +0900

    Boj1010 풀이 중 입니다!

---
+++ Baekjoon/week_04/boj1010.java
@@ -0,0 +1,20 @@
+package Baekjoon.week_04;
+import java.util.Scanner;
+
+public class boj1010 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int T = scan.nextInt();
+        int[] N = new int[T];
+        int[] M = new int[T];
+        int[] count = new int[T];
+        for (int i=0; i<T; i++) {
+            N[i] = scan.nextInt();
+            M[i] = scan.nextInt();
+        }
+        if (N.length <= M.length) {
+
+        }
+    }
+}
